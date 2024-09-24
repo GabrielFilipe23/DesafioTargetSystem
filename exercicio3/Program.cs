691f0af3a389bf91994df9e4d48f8f78b1f81b82
GabrielFilipe23 <gabrielcondee02@gmai.com>
2024-09-24 00:20:02 -0300

    exercicio3

---
+++ exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int soma = 0;
+        int[] numeros = new int[12];
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            numeros[i] = i + 1;
+        }
+
+        for (int K = 0; K < numeros.Length; K++)
+        {
+            soma += numeros[K];
+        }
+
+        Console.WriteLine(soma);
+    }
+}
