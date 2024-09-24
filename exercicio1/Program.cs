b73f2921a28f334004d8345870da6c597e5ba321
GabrielFilipe23 <gabrielcondee02@gmai.com>
2024-09-23 23:53:23 -0300

    exercicio1

---
+++ exercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        int a = 0;
+        int b = 1;
+
+        Console.Write("A sequência de Fibonacci do número fornecido é: ");
+        Console.Write("{0}, {1}", a, b);
+
+        bool pertence = (numero == a || numero == b);
+
+        while (true)
+        {
+            int proximo = a + b;
+            if (proximo > numero) break;
+
+            Console.Write(", {0}", proximo);
+            a = b;
+            b = proximo;
+
+            if (proximo == numero) pertence = true;
+        }
+
+        Console.WriteLine();
+
+        if (pertence)
+        {
+            Console.WriteLine("{0} pertence à sequência de Fibonacci.", numero);
+        }
+        else
+        {
+            Console.WriteLine("{0} não pertence à sequência de Fibonacci.", numero);
+        }
+    }
+}
---
+++ exercicio1/README.md
@@ -0,0 +1,11 @@
+Avaliação de Estágio - Target System
+Requisitos para Funcionamento do Código:
+.NET 8.0 SDK
+Como Executar o Código:
+Acesse o Diretório do Exercício Desejado:
+
+cd <diretorio>
+
+Comando para Executar:
+
+dotnet run
