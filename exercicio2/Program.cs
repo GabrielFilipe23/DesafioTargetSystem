0ed73d7abc0d9b63e6ec050523d8eadcd77ee1a0
GabrielFilipe23 <gabrielcondee02@gmai.com>
2024-09-24 00:13:35 -0300

    exercicio2

---
+++ exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma frase: ");
+        string frase = Console.ReadLine();
+
+        int procurado = 0;
+
+        for (int i = 0; i < frase.Length; i++)
+        {
+            if (frase[i] == 'a' || frase[i] == 'A')
+            {
+                procurado++;
+            }
+        }
+
+        if (procurado > 0)
+        {
+            Console.WriteLine("A letra 'a'  aparece {0} vez(es) na frase.", procurado);
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a'  não aparece na frase.");
+        }
+    }
+}
